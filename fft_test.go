bfbf5b4e9d392984572bea1791b0a6f3f8a36a3c
Jonathan Wills <runningwild@gmail.com>
2011-10-01 09:36:32 -0700

    DFT is working for arbitrary start and stride

--- fft_test.go
+++ fft_test.go
@@ -10,30 +10,69 @@ import (
 )
 
 func NaiveSpec(c gospec.Context) {
-  in := make([]complex128, 256*4096)
+  in := make([]complex128, 8)
   for i := range in {
     in[i] = complex(math.Cos(float64(i) / float64(len(in)) * 2 * math.Pi), 0)
   }
-  fft.FFT(in)
-  return
-  c.Specify("Test basic DFT", func() {
-    in := make([]complex128, 16)
-    for i := range in {
-      in[i] = complex(math.Cos(float64(i) / float64(len(in)) * 2 * math.Pi), 0)
-    }
-    out := make([]complex128, len(in))
-    fft.DFT(in, out, 0, 1)
-    for i := range out {
-      mag,_ := cmath.Polar(out[i])
-      if i == 1 || i == len(out) - 1 {
-        c.Expect(mag, IsWithin(1e-9), float64(len(out))/2)
-      } else {
-        c.Expect(mag, IsWithin(1e-9), 0.0)
+  verify := func(out []complex128, name string) {
+    c.Specify(name, func() {
+//      fmt.Printf("name: %s\n", name)
+//      for i := range out {
+//        fmt.Printf("oot %d: %2.2f\n", i, out[i])
+//      }
+      for i := range out {
+        mag,ang := cmath.Polar(out[i])
+        if i == 1 || i == len(out) - 1 {
+          c.Expect(mag, IsWithin(1e-9), float64(len(out))/2)
+          if real(out[i]) < 0 {
+            if ang < 0 {
+              c.Expect(ang, IsWithin(1e-9), -math.Pi)
+            } else {
+              c.Expect(ang, IsWithin(1e-9), math.Pi)
+            }
+          } else {
+            c.Expect(ang, IsWithin(1e-9), 0.0)
+          }
+        } else {
+          c.Expect(mag, IsWithin(1e-9), 0.0)
+        }
       }
+    })
+  }
+
+  c.Specify("Test basic FFT", func() {
+    out := make([]complex128, len(in))
+    fft.FFT(in, out, 0, 1)
+      for i := range out {
+        fmt.Printf("oot %d: %2.2f\n", i, out[i])
+      }
+      fmt.Printf("\n\n")
+  })
+
+  c.Specify("Test basic DFT", func() {
+    out := make([]complex128, len(in))
+
+    fft.DFT(in, out, 0, 1)
+    verify(out, "Start/Stride 0/1")
+
+    in2 := make([]complex128, 2*len(in))
+    for i := range in2 {
+      in2[i] = in[i/2]
     }
-    out2 := fft.FFT(in)
-    for i := range in {
-      fmt.Printf("%d: %2.2f %2.2f\n", i, out[i], out2[i])
+    fft.DFT(in2, out, 0, 2)
+    verify(out, "Start/Stride 0/2")
+
+
+    fft.DFT(in2, out, 1, 2)
+    verify(out, "Start/Stride 1/2")
+
+    in5 := make([]complex128, len(in)*5)
+    for i := range in5 {
+      in5[i] = in[i/5]
+    }
+    for i := 0; i < 5; i++ {
+      fft.DFT(in5, out, i, 5)
+      verify(out, fmt.Sprintf("Start/Stride %d/%d", i, len(in5)/len(in)))
     }
   })
 }
--- naive.go
+++ naive.go
@@ -2,6 +2,7 @@ package fft
 
 import (
   "math"
+  "fmt"
   "cmath"
 )
 
@@ -13,36 +14,24 @@ const j = complex(0.0, 1.0)
 func DFT(in,out []complex128, start,stride int) {
   N := len(in) / stride
   factor := -2 * math.Pi * complex(0,1) / complex(float64(N),0)
-  for i := range out {
-    out[i] = 0
-  }
-  index := 0
-  for k := range out {
+  for k := 0; k < N; k++ {
+    out[k] = 0
     for n := start; n < len(in); n += stride {
-      out[k] += in[n] * cmath.Exp(factor * complex(float64((k * n) / stride), 0))
+      out[k] += in[n] * cmath.Exp(factor * complex(float64(k * (n / stride)), 0))
     }
-    index++
   }
 }
 
-func FFT(in []complex128) (out []complex128) {
-  out = make([]complex128, len(in))
+func FFT(in,out []complex128, start,stride int) {
   N := len(out)
   if N % 2 != 0 {
-    DFT(in, out, 0, 1)
+    DFT(in, out, start, stride)
     return
   }
-  i0 := make([]complex128, N/2)
-  i1 := make([]complex128, N/2)
-  for i := range i0 {
-    i0[i] = in[2*i]
-    i1[i] = in[2*i+1]
-  }
-  o1 := FFT(i0)
-  o2 := FFT(i1)
-  for i := range o1 {
-    out[i] = o1[i]
-    out[i+N/2] = o2[i]
+  DFT(in, out[ : N/2], start, stride*2)
+  DFT(in, out[N/2 : ], start+stride, stride*2)
+  for i := range out { 
+    fmt.Printf("%d: %2.2f\n", i, out[i])
   }
   factor := -2 * math.Pi * j * complex(1.0 / float64(N), 0.0)
   for k := 0; k < N/2; k ++ {
